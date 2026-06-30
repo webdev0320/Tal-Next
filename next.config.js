6112d9cf2b6717c3ea7017a56ad1459305ca4a6e
Muhammad Arbaz <muhammadarbaz894@gmail.com>
2026-06-30 17:42:00 +0500

    updated code

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const require = createRequire(import.meta.url);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,    // Next.js default image optimization requires a live Node.js server
+  },
   
   // webpack config removed as it broke wp-content.js file imports
 };
